ab349ff4b4ceed2aa7ffbc7aa382c4eeea645aa9
hyungsung <hyungsung@sindoh.com>
2025-09-19 14:45:38 +0900

    (home) ui 작업 중

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,58 +1,43 @@
 import Link from 'next/link';
-import styles from '@/app/components/home.module.css';
 import Image from 'next/image';
+import { Button,Switch } from 'antd';
+import mainImage from '@/app/images/mainImage.png';
 import 'material-icons/iconfont/material-icons.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div id="header" className="static flex h-[5rem] shrink-0 bg-lime-900 text-lime-50 p-[1rem] flex-row justify-evenly items-center">
-        <div id="select_user_type">
-          <div>Company</div>
-          
-          <div>Personal</div>
+      <div id="header" className="static flex flex-col flex-0 bg-[#142131] text-lime-50 p-[1rem] justify-center items-center">
+        <div id="logo" className='justify-center items-center mt-5 mb-3'>
+          <Image 
+            src="/cloudMpsLogo.png"
+            width={150}
+            height={105}
+            alt='Cloud MPS Logo'
+          />
         </div>
-        <div id="title" className='text-2xl font-semibold'>Cloud MPS</div>
-        <div id="select_process" className='gap-2'>
-          <button className='border-2 border-lime-100 rounded-sm'>Login</button>
-          <button className='border-2 border-lime-100 rounded-sm'>Register</button>
+        <div id="title" className='text-4xl font-semibold'>Cloud MPS</div>
+        <div id="subItems" className='w-full flex mx-10 my-5 flex-row justify-between items-center'>
+          <div id="selectUserType">
+            <div>
+              Company <Switch /> Personal
+            </div>
+          </div>
+          <div id="selectInfo" className='flex text-xl flex-row justify-between items-center gap-4'>
+            <div id="intro">Introduction</div>
+            <div id="doc">Documentation</div>
+          </div>
+          <div id="select_process" className='flex gap-2'>
+            <Button className='border-2 border-lime-100 rounded-sm'>Login</Button>
+            <Button className='border-2 border-lime-100 rounded-sm'>Register</Button>
+          </div>
         </div>
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div
-          className={styles.shape}
-        />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span>
-            <span className="material-icons w-5 md:w-6" >arrow_left</span>
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+      <div className="flex grow flex-col md:flex-row justify-center items-center bg-[#142131]">
+        <div id="mainImage" className='w-full'>
           <Image 
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className='hidden md:block'
-            alt='Screenshots of the dashboard project showing desktop version'
-          />
-          <Image 
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className='block md:hidden'
-            alt='Screenshots of the dashboard project showing desktop version'
+            src={mainImage}
+            alt='Improve your business with Cloud MPS'
           />
         </div>
       </div>
