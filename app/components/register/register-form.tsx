83ce73c4e257f6c08bb917bbd0ab2b026f8a2ed4
hyungsung <newtons2002@gmail.com>
2025-10-15 22:55:18 +0900

    (register) 인증 제거

--- app/components/register/register-form.tsx
+++ app/components/register/register-form.tsx
@@ -1,22 +1,19 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import { z } from "zod";
 import { Steps } from "antd";
 
 import { BASE_PATH } from "@/app/libs/constants";
 import RegisterTermsOfService from "./register-use-of-term";
 import RegisterUserInfo, { SearchCompnayDataType } from "./register-user-info";
-import RegisterVerification from "./register-verification";
 import { fetchIp, register } from "@/app/libs/actions";
-
 
 enum RegisterStep {
   AGREEMENT = 0,
   INFORMATION = 1,
-  VERIFICATION = 2,
-  COMPLETE = 3,
+  COMPLETE = 2,
 }
 
 export type AgreementState = {
@@ -52,13 +49,6 @@ export type RegisterCompanyUserState = {
   message?: string | null;
 };
 
-export type RegisterVerificationState = {
-  errors?: {
-    verificationCode?: string[];
-  };
-  message?: string | null;
-};
-
 export default function RegisterForm({
   userType,
   searchResult,
@@ -67,29 +57,31 @@ export default function RegisterForm({
 }: {
   userType: "company" | "person";
   searchResult: SearchCompnayDataType[] | null;
-  trans: { company: Record<string, string>,
-    register: Record<string, string>,
-    user: Record<string, string>
+  trans: {
+    company: Record<string, string>;
+    register: Record<string, string>;
+    user: Record<string, string>;
   };
   terms: {
     terms_of_service: string[][];
     privacy_policy: string[][];
     location_info_policy: string[][];
     event_promotion_policy: string[][];
-  }
+  };
 }) {
   const [agreed, setAgreed] = useState({
-    termsOfService : false,
+    termsOfService: false,
     privacyPolicy: false,
     locationInfoPolicy: false,
     eventPromotionPolicy: false,
   });
 
   const [userIp, setUserIp] = useState<string>("");
-  const [verifyCode, setVerifyCode] = useState<string>("");
-  const [companyCode, setCompanyCode] = useState<string>("");
 
-  const loginLink = userType === "company" ? "/login?userType=company" : "/login?userType=person";
+  const loginLink =
+    userType === "company"
+      ? "/login?userType=company&init=true"
+      : "/login?userType=person&init=true";
 
   // control ---------------------------------------------------------------------------
   const [registerStep, setRegisterStep] = useState<RegisterStep>(
@@ -119,11 +111,11 @@ export default function RegisterForm({
     const check_errors: Record<string, string> = {};
     if (!agreeTermsOfService) {
       check_errors["agreeTermsOfService"] = trans.register.error_need_agree;
-    };
-    if(!agreePrivacyPolicy) {
+    }
+    if (!agreePrivacyPolicy) {
       check_errors["agreePrivacyPolicy"] = trans.register.error_need_agree;
-    };
-    if(!agreeLocationInfoPolicy) {
+    }
+    if (!agreeLocationInfoPolicy) {
       check_errors["agreeLocationInfoPolicy"] = trans.register.error_need_agree;
     }
     if (Object.keys(check_errors).length > 0) {
@@ -134,7 +126,7 @@ export default function RegisterForm({
     }
 
     const updatedAgreed = {
-      termsOfService : !!agreeTermsOfService,
+      termsOfService: !!agreeTermsOfService,
       privacyPolicy: !!agreePrivacyPolicy,
       locationInfoPolicy: !!agreeLocationInfoPolicy,
       eventPromotionPolicy: !!agreeEventPromotionPolicy,
@@ -146,67 +138,92 @@ export default function RegisterForm({
 
   // user information ------------------------------------------------------------------
   const PersonalUserFormSchema = z.object({
-    userName: z.string().min(1, { message: trans.user.error_miss_input as string }),
-    userFullName: z.string().min(1, { message: trans.register.error_miss_input as string }),
-    userEmail: z.string().email({ message: trans.register.error_input_type_email as string }),
+    userName: z
+      .string()
+      .min(1, { message: trans.user.error_miss_input as string }),
+    userFullName: z
+      .string()
+      .min(1, { message: trans.register.error_miss_input as string }),
+    userEmail: z
+      .string()
+      .email({ message: trans.register.error_input_type_email as string }),
     userPwdNew: z.string().min(6, {
-      message: trans.register.error_pwd_min_legnth as string
+      message: trans.register.error_pwd_min_legnth as string,
     }),
     userPwdNewAgain: z.string().min(6, {
-      message: trans.register.error_pwd_min_legnth as string
-    })
+      message: trans.register.error_pwd_min_legnth as string,
+    }),
   });
 
   const CompanyUserFormSchema = z.object({
     companyType: z.enum(["GENERAL", "PARTNER"]),
-    companyName: z.string().trim().min(1, {
-      message: trans.register.error_miss_input as string
-    }),
+    companyName: z
+      .string()
+      .trim()
+      .min(1, {
+        message: trans.register.error_miss_input as string,
+      }),
     companyRegistrationNo: z.string().min(1, {
-      message: trans.register.error_miss_input as string
+      message: trans.register.error_miss_input as string,
     }),
     dealCompanyCode: z.string(),
-    ceoName: z.string().min(1, { message: trans.user.error_miss_input as string }),
+    ceoName: z
+      .string()
+      .min(1, { message: trans.user.error_miss_input as string }),
     timeZone: z.string(),
     companyCountry: z.string(),
     language: z.string(),
     currencyCode: z.string(),
     companyBusinessItem: z.string(),
     companyBusinessType: z.string(),
-    userFullName: z.string().min(1, { message: trans.user.error_miss_input as string }),
+    userFullName: z
+      .string()
+      .min(1, { message: trans.user.error_miss_input as string }),
     userEmail: z.email({
-        error: trans.register.error_input_type_email as string,
-      }),
+      error: trans.register.error_input_type_email as string,
+    }),
     userPwdNew: z.string().min(6, {
-      error: (issue) => issue.input === undefined ?
-        trans.register.error_miss_input as string:
-        trans.register.error_pwd_min_legnth as string
-      }),
+      error: (issue) =>
+        issue.input === undefined
+          ? (trans.register.error_miss_input as string)
+          : (trans.register.error_pwd_min_legnth as string),
+    }),
     userPwdNewAgain: z.string().min(6, {
-      error: (issue) => issue.input === undefined ?
-        trans.register.error_miss_input as string:
-        trans.register.error_pwd_min_legnth as string
-      })
+      error: (issue) =>
+        issue.input === undefined
+          ? (trans.register.error_miss_input as string)
+          : (trans.register.error_pwd_min_legnth as string),
+    }),
   });
 
   const CompanyUserFormSchema2 = z.object({
-    companyCode: z.string().trim().min(1, { message: trans.user.error_miss_input as string }),
-    companyName: z.string().trim().min(1, { message: trans.user.error_miss_input as string }),
+    companyCode: z
+      .string()
+      .trim()
+      .min(1, { message: trans.user.error_miss_input as string }),
+    companyName: z
+      .string()
+      .trim()
+      .min(1, { message: trans.user.error_miss_input as string }),
     dealCompanyCode: z.string(),
-    userFullName: z.string().min(1, { message: trans.user.error_miss_input as string }),
+    userFullName: z
+      .string()
+      .min(1, { message: trans.user.error_miss_input as string }),
     userEmail: z.email({
-        error: trans.register.error_input_type_email as string,
-      }),
+      error: trans.register.error_input_type_email as string,
+    }),
     userPwdNew: z.string().min(6, {
-      error: (issue) => issue.input === undefined ?
-        trans.register.error_miss_input as string:
-        trans.register.error_pwd_min_legnth as string
-      }),
+      error: (issue) =>
+        issue.input === undefined
+          ? (trans.register.error_miss_input as string)
+          : (trans.register.error_pwd_min_legnth as string),
+    }),
     userPwdNewAgain: z.string().min(6, {
-      error: (issue) => issue.input === undefined ?
-        trans.register.error_miss_input as string:
-        trans.register.error_pwd_min_legnth as string
-      })
+      error: (issue) =>
+        issue.input === undefined
+          ? (trans.register.error_miss_input as string)
+          : (trans.register.error_pwd_min_legnth as string),
+    }),
   });
 
   const actionRegisterUser = async (
@@ -243,34 +260,34 @@ export default function RegisterForm({
     //-----TEST : End ----------------------------------------
     let validatedFields = null;
     if (userType === "company") {
-      const companyCode = formData.get('companyCode');
-      if(!companyCode) {
+      const companyCode = formData.get("companyCode");
+      if (!companyCode) {
         validatedFields = CompanyUserFormSchema.safeParse({
-          companyType: formData.get('companyType'),
-          companyName: formData.get('companyName'),
-          companyRegistrationNo: formData.get('companyRegistrationNo'),
-          ceoName: formData.get('ceoName'),
-          dealCompanyCode: formData.get('dealCompanyCode'),
-          timeZone: formData.get('timeZone'),
-          companyCountry: formData.get('companyCountry'),
-          language: formData.get('language'),
-          currencyCode: formData.get('currencyCode'),
-          companyBusinessItem: formData.get('companyBusinessItem'),
-          companyBusinessType: formData.get('companyBusinessType'),
-          userFullName: formData.get('userFullName'),
-          userEmail: formData.get('userEmail'),
-          userPwdNew: formData.get('userPwdNew'),
-          userPwdNewAgain: formData.get('userPwdNewAgain'),
+          companyType: formData.get("companyType"),
+          companyName: formData.get("companyName"),
+          companyRegistrationNo: formData.get("companyRegistrationNo"),
+          ceoName: formData.get("ceoName"),
+          dealCompanyCode: formData.get("dealCompanyCode"),
+          timeZone: formData.get("timeZone"),
+          companyCountry: formData.get("companyCountry"),
+          language: formData.get("language"),
+          currencyCode: formData.get("currencyCode"),
+          companyBusinessItem: formData.get("companyBusinessItem"),
+          companyBusinessType: formData.get("companyBusinessType"),
+          userFullName: formData.get("userFullName"),
+          userEmail: formData.get("userEmail"),
+          userPwdNew: formData.get("userPwdNew"),
+          userPwdNewAgain: formData.get("userPwdNewAgain"),
         });
       } else {
         validatedFields = CompanyUserFormSchema2.safeParse({
-          companyCode: formData.get('companyCode'),
-          companyName: formData.get('companyName'),
-          dealCompanyCode: formData.get('dealCompanyCode'),
-          userFullName: formData.get('userFullName'),
-          userEmail: formData.get('userEmail'),
-          userPwdNew: formData.get('userPwdNew'),
-          userPwdNewAgain: formData.get('userPwdNewAgain'),
+          companyCode: formData.get("companyCode"),
+          companyName: formData.get("companyName"),
+          dealCompanyCode: formData.get("dealCompanyCode"),
+          userFullName: formData.get("userFullName"),
+          userEmail: formData.get("userEmail"),
+          userPwdNew: formData.get("userPwdNew"),
+          userPwdNewAgain: formData.get("userPwdNewAgain"),
         });
       }
     } else {
@@ -280,7 +297,7 @@ export default function RegisterForm({
         userPwdNew: formData.get("userPwdNew"),
         userPwdNewAgain: formData.get("userPwdNewAgain"),
       });
-    };
+    }
 
     if (!!validatedFields && validatedFields.error) {
       const tree = z.treeifyError(validatedFields.error);
@@ -289,112 +306,57 @@ export default function RegisterForm({
         errors: tree.properties,
         message: trans.register.errors_in_inputs,
       } as RegisterPersonalUserState | RegisterCompanyUserState;
-    };
+    }
 
     // check if passwords are same ----------------------------------------
     const { userPwdNew, userPwdNewAgain } = validatedFields.data;
     if (userPwdNew !== userPwdNewAgain) {
       return {
-        errors: [
-          {userPwdNew: trans.register.error_pwd_mismatch}
-        ],
+        errors: [{ userPwdNew: trans.register.error_pwd_mismatch }],
         message: trans.register.error_pwd_mismatch,
       } as RegisterPersonalUserState | RegisterCompanyUserState;
-    };
+    }
 
     const registerData = {
-      user_type : userType.toUpperCase(),
-      company_type : formData.get("companyType"),
-      company_name : formData.get("companyName"),
-      business_registration_code : formData.get("companyRegistrationNo"),
-      company_code : formData.get("companyCode"),
-      deal_company_code : formData.get("dealCompanyCode"),
-      ceo_name : formData.get("ceoName"),
-      language : formData.get("language"),
-      time_zone : formData.get("timeZone"),
-      currency_code : formData.get("currencyCode"),
-      country : formData.get("companyCountry"),
-      terms_of_service : agreed.termsOfService ? "Y" : "N",
-      privacy_policy : agreed.privacyPolicy ? "Y" : "N",
-      location_information : agreed.locationInfoPolicy ? "Y" : "N",
-      notification_email : agreed.eventPromotionPolicy ?  "Y" : "N",
-      full_name :  formData.get("userFullName"),
-      e_mail_adress : formData.get("userEmail"),
-      password : formData.get("userPwdNew"),
-      ip_address : userIp,
+      user_type: userType.toUpperCase(),
+      company_type: formData.get("companyType"),
+      company_name: formData.get("companyName"),
+      business_registration_code: formData.get("companyRegistrationNo"),
+      company_code: formData.get("companyCode"),
+      deal_company_code: formData.get("dealCompanyCode"),
+      ceo_name: formData.get("ceoName"),
+      language: formData.get("language"),
+      time_zone: formData.get("timeZone"),
+      currency_code: formData.get("currencyCode"),
+      country: formData.get("companyCountry"),
+      terms_of_service: agreed.termsOfService ? "Y" : "N",
+      privacy_policy: agreed.privacyPolicy ? "Y" : "N",
+      location_information: agreed.locationInfoPolicy ? "Y" : "N",
+      notification_email: agreed.eventPromotionPolicy ? "Y" : "N",
+      full_name: formData.get("userFullName"),
+      e_mail_adress: formData.get("userEmail"),
+      password: formData.get("userPwdNew"),
+      ip_address: userIp,
     };
 
-    console.log('Register / data :', registerData);
+    // console.log('Register / data :', registerData);
     const result = await register(registerData);
-    if(!!result) {
-      if(result["ResultCode"] === 0) {
+    if (!!result) {
+      if (result["ResultCode"] === 0) {
         // Succeeded ---------------------------
-        if(!!result["verification_code"]) {
-          setVerifyCode(result["verification_code"]);
-        };
-        if(!!result["company_code"]) {
-          setCompanyCode(result["company_code"]);
-        };
         handleNextStep();
         return;
       } else {
         return {
-          message: result["ErrorMessage"]
-        } as RegisterCompanyUserState | RegisterPersonalUserState;  
-      };
+          message: result["ErrorMessage"],
+        } as RegisterCompanyUserState | RegisterPersonalUserState;
+      }
     } else {
       return {
-        message: "No Response from server"
+        message: "No Response from server",
       } as RegisterCompanyUserState | RegisterPersonalUserState;
     }
   };
-
-  // user verification ------------------------------------------------------------------
-  const actionVerification = async (
-    prevState: void | RegisterVerificationState,
-    formData: FormData
-  ) => {
-    console.log("actionVerification called");
-    const userFullName = formData.get("userFullName");
-    const userEmail = formData.get("userEmail");
-    const userPwd = formData.get("userPwd");
-
-    const code1 = formData.get("code_1st");
-    const code2 = formData.get("code_2nd");
-    const code3 = formData.get("code_3rd");
-    const code4 = formData.get("code_4th");
-    const code5 = formData.get("code_5th");
-    const code6 = formData.get("code_6th");
-
-    const verificationCode = `${code1}${code2}${code3}${code4}${code5}${code6}`;
-    console.log("actionVerification / verificationCode :", verificationCode);
-
-    if(verificationCode !== verifyCode) {
-    } else {
-      try {
-        const resp = await fetch(`${BASE_PATH}/user/login_vericode`, {
-          method: 'post',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            user_name: userFullName,
-            user_email: userEmail,
-            user_pwd: userPwd,
-            verification_code: verificationCode,
-            company_code: companyCode
-          })
-        });
-
-        const result = await resp.json();
-        if(!!result) {
-          if(result["ResultCode"] === 0) {
-            handleNextStep();
-          }
-        };
-      } catch(err) {
-        console.error(`\t[ Regsiter ] Error : ${err}`);
-      }
-    }
-  }
 
   const registerSteps = [
     {
@@ -422,21 +384,20 @@ export default function RegisterForm({
       ),
     },
     {
-      title: trans.register.verification,
-      content: (
-        <RegisterVerification
-          trans={trans}
-          action={actionVerification}
-          goback={handlePrevStep}
-        />
-      ),
-    },
-    {
       title: trans.register.complete,
-      content: <>
-        <div className="p-8 flex justify-center items-center text-2xl">{trans.register.complete}</div>
-        <Link href={loginLink} className="p-4 flex justify-center items-center text-blue-500">{trans.register.login_next}</Link>
-      </>,
+      content: (
+        <>
+          <div className="p-8 flex justify-center items-center text-2xl">
+            {trans.register.complete}
+          </div>
+          <Link
+            href={loginLink}
+            className="p-4 flex justify-center items-center text-blue-500"
+          >
+            {trans.register.go_to_verify}
+          </Link>
+        </>
+      ),
     },
   ];
 
