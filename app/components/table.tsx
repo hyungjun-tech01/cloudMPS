77060d0be1bf5341563fef12ca6489421c255675
hyungsung <hyungsung@mylation.com>
2026-03-07 08:58:30 +0900

    antd 제거 (일단)

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import * as React from 'react';
-import { Table } from 'antd';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from '@mui/material';
+import Paper from '@mui/material/Paper';
 import type { IColumn } from '@/app/libs/types';
 
 
@@ -19,10 +27,28 @@ export default function CustomizedTable({
 }: ITable) {
 
     return (
-        <Table
-            columns={columns}
-            dataSource={dataSource}
-            scroll={{ x: 'max-content' }}
-        />
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        {columns.map((column) => (
+                            <TableCell key={column.key}>{column.title}</TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {dataSource.map((row) => (
+                        <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
